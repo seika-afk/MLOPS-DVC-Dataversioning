a7ce3f585361c8c78bf3f73b24fbcde65e0ee9d9
Seika <artistnoob6@gmail.com>
2025-07-04 19:21:19 +0530

    v3

--- data_save.py
+++ data_save.py
@@ -16,7 +16,7 @@ data = {
         'Yukihiro Matsumoto',
         'Anders Hejlsberg',
         'JetBrains',
-        'Apple Incorporation'
+        'Apple Incorporation-Maybe'
     ],
     'Typing Discipline': [
         'Duck / Dynamic',
@@ -53,4 +53,4 @@ os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'sample_data.csv')
 df.to_csv(file_path,index=False)
 
-print(f"csv fike saved to {file_path}")
+print(f"csv file saved to {file_path}")
